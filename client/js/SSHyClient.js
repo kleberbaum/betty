8a0c6a0b69b8581b37ee4b82d1def951bd5d8343
Florian Kleber <kleber@snek.at>
2024-09-18 15:05:18 +0000

    refactor: Update string encoding and decoding methods in SSHyClient.js

--- client/js/SSHyClient.js
+++ client/js/SSHyClient.js
@@ -16,16 +16,11 @@ var wsproxyEncoding = null;
 // https://developers.google.com/web/updates/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
 //
 function ab2str(buf) {
-  return String.fromCharCode.apply(null, new Uint8Array(buf));
+  return new TextDecoder().decode(new Uint8Array(buf));
 }
 
 function str2ab(str) {
-  var buf = new ArrayBuffer(str.length * 1); // 2 bytes for each char
-  var bufView = new Uint8Array(buf);
-  for (var i = 0, strLen = str.length; i < strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
-  }
-  return buf;
+  return new TextEncoder().encode(str).buffer;
 }
 
 // ==============
@@ -142,6 +137,7 @@ function startSSHy() {
   };
   // Just a little abstraction from ws.send
   ws.sendB64 = function (e) {
+    console.log("sendB64", e);
     if (wsproxyEncoding == 'binary') {
       this.send(str2ab(e));
 
